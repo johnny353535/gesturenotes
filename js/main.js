71dd309a7fd8d0870463bb64843f4ddb4966c9df
Jonas <elmos-erbe@web.de>
2013-05-31 22:34:52 +0200

    Removed swipe, cleanup, style changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('.slides>li').hammer().on('dragleft dragright dragdown swipeleft swiperight release', handleGestures);
+    $('.slides>li').hammer({drag_lock_to_axis: true}).on('dragleft dragright dragdown release', handleGestures);
 
 })
 
@@ -33,28 +33,10 @@ function handleGestures(ev){
 
             break;
 
-        case 'swiperighta': // o-->
-            if(_this.is(':last-child')) break;
-
-            console.log('swiperight');
-
-            prev(_this);
-            ev.gesture.stopDetect();
-
-            break;
-
-        case 'swipelefta': // <--o
-            if(_this.is(':first-child')) break;
-
-            console.log('swipeleft');
-
-            next(_this);
-            ev.gesture.stopDetect();
-
-            break;
-
         case 'dragdown':
             if(_this.is(':first-child') || _this.is(':last-child')) break;
+
+            console.log('dragdown');
 
             if(ev.gesture.deltaY < 60) _this.css('-webkit-transform', 'translate3d(0,'+ev.gesture.deltaY+'px,0)');
 
@@ -66,36 +48,24 @@ function handleGestures(ev){
 
             if(ev.gesture.direction == 'right') {
                 prev(_this);
-            } else {
+            } else if(ev.gesture.direction == 'left') {
                 next(_this);
+            } else {
+                reset(_this);
             }
-
-            /*
-            if(Math.abs(ev.gesture.deltaX) > _this.outerWidth()/5) {
-                if(ev.gesture.direction == 'right') {
-                    prev(_this);
-                } else {
-                    next(_this);
-                }
-            }
-            else { // reset
-                if(ev.gesture.direction == 'right') {
-                    _this.next().css('-webkit-transform', 'translate3d(-'+_this.outerWidth()+'px,0,0)');
-                } else {
-                    _this.css('-webkit-transform', 'translate3d(0,0,0)');
-                }
-            }
-            */
-
 
             break;
     }
 }
 
 function next(elem){
-    elem.removeClass('noAnimation').css('-webkit-transform', 'translate3d(-'+elem.outerWidth()+'px,0,0)');
+    elem.css('-webkit-transform', 'translate3d(-'+elem.outerWidth()+'px,0,0)');
 }
 
 function prev(elem){
-    elem.next().show().removeClass('noAnimation').css('-webkit-transform', 'translate3d(0,0,0)');
+    elem.next().show().css('-webkit-transform', 'translate3d(0,0,0)');
+}
+
+function reset(elem){
+    elem.css('-webkit-transform', 'translate3d(0,0,0)');
 }
